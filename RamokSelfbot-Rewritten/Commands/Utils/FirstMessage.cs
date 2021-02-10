8bf2fede77bc47de8f29622a45db24907ef0b8a3
RamokTVL <63268168+RamokTVL@users.noreply.github.com>
2021-02-10 22:47:06 +0100

    Added FirstMessage command !

---
+++ RamokSelfbot-Rewritten/Commands/Utils/FirstMessage.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using Discord;
+using System;
+using System.Linq;
+
+namespace RamokSelfbot.Commands.Utils
+{
+    [Command("firstmessage", "Get the first message of the channel - UTILS")]
+    class FirstMessage : CommandBase
+    {
+        public override void Execute()
+        {
+            if (RamokSelfbot.Utils.IsClient(Message))
+            {
+                RamokSelfbot.Utils.SendEmbed(Message, new EmbedMaker()
+                {
+                    Title = "First Message",
+                    Description = "[Jump](http://discord.com/channels/" + Message.Guild.Id + "/" + Message.Channel.Id + "/" + Message.Channel.GetMessages().Reverse().First().Id + ")"
+                });
+            }
+        }
+    }
+}
