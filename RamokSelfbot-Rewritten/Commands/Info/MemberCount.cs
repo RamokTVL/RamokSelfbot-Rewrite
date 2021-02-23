54c8f7c374057743af07743e917e036b3e3bdfce
RamokTVL <steamxaiden@gmail.com>
2021-02-23 17:51:31 +0100

    Added MemberCount command

---
+++ RamokSelfbot-Rewritten/Commands/Info/MemberCount.cs
@@ -0,0 +1,15 @@
+using Discord;
+using Discord.Commands;
+using Discord.Gateway;
+using System;
+
+namespace RamokSelfbot_Rewritten.Commands.Info
+{
+    [Command("membercount", "Show the members in the server - INFO")]
+    public class MemberCount : CommandBase
+    {
+        public override Execute() {
+            if(Message)
+        }
+    }
+}
