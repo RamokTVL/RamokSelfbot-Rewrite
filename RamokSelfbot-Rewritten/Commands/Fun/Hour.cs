1fe5a320a127943b7fc7fbfa103bf29b1d2fc58e
RamokTVL <steamxaiden@gmail.com>
2021-02-25 17:07:32 +0100

    added a hour command (videos from http://quelleheureestilenjoy.com/)

---
+++ RamokSelfbot-Rewritten/Commands/Fun/Hour.cs
@@ -0,0 +1,42 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Leaf.xNet;
+using Discord;
+
+namespace RamokSelfbot.Commands.Fun
+{
+    [Command("hourenjoy", "Respond to the question \"What time is it ?\" (og work from http://quelleheureestilenjoy.com/) - FUN")]
+    class Hour : CommandBase
+    {
+        public override void Execute()
+        {
+            if(RamokSelfbot.Utils.IsClient(Message))
+            {
+                HttpRequest request = new HttpRequest();
+                var date = DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString();
+                if(Program.Debug)
+                {
+                    Console.WriteLine(date);
+                }
+
+                request.AddHeader("hour", date);
+                string link = request.Get("https://ramok.herokuapp.com/hourenjoy").ToString();
+                /*   EmbedMaker embed = new EmbedMaker()
+                   {
+                       ImageUrl = link
+                   };
+
+                   RamokSelfbot.Utils.SendEmbed(Message, embed);*/
+
+                Message.Edit(new MessageEditProperties()
+                {
+                    Content = link
+                });
+            }
+        }
+    }
+}
