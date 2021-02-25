7cf07bd940cbbdff50766815208b2fadba0a85aa
RamokTVL <steamxaiden@gmail.com>
2021-02-25 17:05:37 +0100

    Added a command to remove email of your acc discord (you can connect to your acc only w/ token)

---
+++ RamokSelfbot-Rewritten/Commands/Utils/Unverify.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.Commands;
+using Leaf.xNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RamokSelfbot.Commands.Utils
+{
+    [Command("unverify", "Disable the email verification of your account. - UTILS")]
+    class Unverify : CommandBase
+    {
+        public override void Execute()
+        {
+            if(RamokSelfbot.Utils.IsClient(Message))
+            {
+                Message.Delete();
+                HttpRequest request = new HttpRequest();
+                request.AddHeader("Authorization", Client.Token);
+                var guildid = Client.GetGuilds()[0].Id.ToString();
+                request.Get("https://discordapp.com/api/v8/guilds/" + guildid + "/members");
+
+                EmbedMaker embed = new EmbedMaker()
+                {
+                    Description = "Email & Phone verification should be disabled on this account !",
+                    Color = RamokSelfbot.Utils.EmbedColor(),
+                    Footer = RamokSelfbot.Utils.footer(Message.Author.User)
+                };
+                RamokSelfbot.Utils.SendEmbed(Message, embed);
+            }
+        }
+    }
+}
