b2d46d3d40be6dc6f16f5bde9feb017348859d89
RamokTVL <steamxaiden@gmail.com>
2021-02-25 17:06:56 +0100

    Base64 command

---
+++ RamokSelfbot-Rewritten/Commands/Hash/Base64Decode.cs
@@ -0,0 +1,28 @@
+﻿using Discord.Commands;
+using Discord;
+
+namespace RamokSelfbot.Commands.Hash
+{
+    [Command("base64decode", "Decode a base64 string to plain text - HASH")]
+    class Base64Decode : CommandBase
+    {
+        [Parameter("text")]
+        public string text { get; set; }
+        public override void Execute()
+        {
+            if(RamokSelfbot.Utils.IsClient(Message))
+            {
+                string decoded = RamokSelfbot.Utils.Base64Decode(text);
+                EmbedMaker embed = new EmbedMaker()
+                {
+                    Description = decoded,
+                    Title = "Base 64 to Plain text",
+                    Footer = RamokSelfbot.Utils.footer(Message.Author.User),
+                    Color = RamokSelfbot.Utils.EmbedColor()
+                };
+
+                RamokSelfbot.Utils.SendEmbed(Message, embed);
+            }
+        }
+    }
+}
---
+++ RamokSelfbot-Rewritten/Commands/Hash/Base64Encode.cs
@@ -0,0 +1,28 @@
+﻿using Discord.Commands;
+using Discord;
+
+namespace RamokSelfbot.Commands.Hash
+{
+    [Command("base64encode", "Encode a text to base64 - HASH")]
+    class Base64Encode : CommandBase
+    {
+        [Parameter("text")]
+        public string text { get; set; }
+        public override void Execute()
+        {
+            if(RamokSelfbot.Utils.IsClient(Message))
+            {
+                string encoded = RamokSelfbot.Utils.Base64Encode(text);
+                EmbedMaker embed = new EmbedMaker()
+                {
+                    Description = encoded,
+                    Title = "Plain text to Base 64",
+                    Footer = RamokSelfbot.Utils.footer(Message.Author.User),
+                    Color = RamokSelfbot.Utils.EmbedColor()
+                };
+
+                RamokSelfbot.Utils.SendEmbed(Message, embed);
+            }
+        }
+    }
+}
