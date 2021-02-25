b5e2b9c1f08df4d72f2d4eab6123a8ec23f365d7
RamokTVL <steamxaiden@gmail.com>
2021-02-25 17:05:55 +0100

    Added a Hypesquad changer (glitched but working)

---
+++ RamokSelfbot-Rewritten/Commands/Utils/Hypesquad.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System.Threading;
+
+namespace RamokSelfbot.Commands.Utils
+{
+    [Command("hypesquad", "Allow you to change your hypesquad - UTILS")]
+    class Hypesquad : CommandBase
+    {
+        public override void Execute()
+        {
+            if (Message.Author.User.Id == Program.id)
+            {
+                    Message.Delete();
+                    new Thread(new ThreadStart(OpenForm)).Start();
+            }
+        }
+
+        public static void OpenForm()
+        {
+            new Forms.Hypesquad().ShowDialog();
+        }
+    }
+}
