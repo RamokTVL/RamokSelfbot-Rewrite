418f26dc1b9c27e0967e36664671690360b024fd
RamokTVL <steamxaiden@gmail.com>
2021-02-25 17:06:21 +0100

    HypeSquad GUI

---
+++ RamokSelfbot-Rewritten/Commands/Utils/Forms/Hypesquad.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace RamokSelfbot.Commands.Utils.Forms
+{
+    partial class Hypesquad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "None",
+            "Brilliance",
+            "Bravery",
+            "Balance"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 87);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(359, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 114);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(358, 78);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Hypesquad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 204);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Hypesquad";
+            this.Sizable = false;
+            this.Text = "Hypesquad";
+            this.Load += new System.EventHandler(this.GUI_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ RamokSelfbot-Rewritten/Commands/Utils/Forms/Hypesquad.cs
@@ -0,0 +1,83 @@
+﻿using MaterialSkin;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RamokSelfbot.Commands.Utils.Forms
+{
+    public partial class Hypesquad : MaterialSkin.Controls.MaterialForm
+    {
+        MaterialSkinManager skinManager = MaterialSkinManager.Instance;
+        public Hypesquad()
+        {
+            InitializeComponent();
+            skinManager.AddFormToManage(this);
+            skinManager.Theme = MaterialSkinManager.Themes.DARK;
+            skinManager.ColorScheme = new ColorScheme(Primary.DeepPurple500, Primary.Grey900, Primary.Grey900, Accent.DeepPurple400, TextShade.WHITE);
+        }
+
+        private void GUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem = comboBox1.SelectedIndex.ToString();
+
+            switch (selectedItem)
+            {
+                case "-1":
+                    richTextBox1.Clear();
+                    richTextBox1.AppendText("Please, set a hypesquad!");
+
+                    // MessageBox.Show("Please, set a hypesquad!", "Hypesquad error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "0":
+                    Program.client.User.SetHypesquad(Discord.Hypesquad.None);
+                    Program.client.User.Update();
+                    richTextBox1.Clear();
+                    richTextBox1.AppendText("Hypesquad set to none!");
+
+                    //MessageBox.Show("Hypesquad set to none!", "Hypesquad success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "1":
+                    Program.client.User.SetHypesquad(Discord.Hypesquad.Brilliance);
+                    Program.client.User.Update();
+                    richTextBox1.Clear();
+                    richTextBox1.AppendText("Hypesquad set to Brilliance!");
+                    return;
+                    //MessageBox.Show("Hypesquad set to Brilliance!", "Hypesquad success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "2":
+                    Program.client.User.SetHypesquad(Discord.Hypesquad.Bravery);
+                    Program.client.User.Update();
+                    richTextBox1.Clear();
+                    richTextBox1.AppendText("Hypesquad set to Bravery!");
+
+                    //  MessageBox.Show("Hypesquad set to Bravery!", "Hypesquad success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+                case "3":
+                    Program.client.User.SetHypesquad(Discord.Hypesquad.Balance);
+                    Program.client.User.Update();
+                    richTextBox1.Clear();
+                    richTextBox1.AppendText("Hypesquad set to Balance!");
+
+                    //    MessageBox.Show("Hypesquad set to Balance!", "Hypesquad success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    break;
+            }
+        }
+    }
+}
