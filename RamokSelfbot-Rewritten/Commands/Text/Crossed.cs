05a474b6d8fff48fd9c4454944da06d0d2c46ed6
RamokTVL <steamxaiden@gmail.com>
2021-03-21 23:13:21 +0100

    Adding crossed command

---
+++ RamokSelfbot-Rewritten/Commands/Text/Crossed.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace RamokSelfbot.Commands.Text
+{
+    [Command("crossed", "Set your text on crossed - TEXT")]
+    class Crossed : CommandBase
+    {
+        [Parameter("text")]
+        public string text { get; set; }
+        public override async void Execute()
+        {
+            if (RamokSelfbot.Utils.IsClient(Message))
+            {
+                await Message.EditAsync(new MessageEditProperties()
+                {
+                    Content = $"~~{text}~~"
+                });
+            }
+        }
+    }
+}
