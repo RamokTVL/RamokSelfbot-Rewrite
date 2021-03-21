3f6ad7242734783cc4759ef3bfdd8ebe9415e6c4
RamokTVL <steamxaiden@gmail.com>
2021-03-21 23:14:50 +0100

    Adding ItalicBoldUnderlined command

---
+++ RamokSelfbot-Rewritten/Commands/Text/ItalicBoldUnderlined.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace RamokSelfbot.Commands.Text
+{
+    [Command("italicboldunderlined", "Set your text on Itatic Bold Underlined - TEXT")]
+    class italicboldunderlined : CommandBase
+    {
+        [Parameter("text", false)]
+        public string text { get; set; }
+        public override async void Execute()
+        {
+            if (RamokSelfbot.Utils.IsClient(Message))
+            {
+                await Message.EditAsync(new MessageEditProperties()
+                {
+                    Content = $"***{text}***"
+                });
+            }
+        }
+    }
+}
