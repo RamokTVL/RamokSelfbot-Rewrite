f07660200883523a42f0ca27abcbb069472d9b8c
RamokTVL <steamxaiden@gmail.com>
2021-03-21 23:16:24 +0100

    Adding Underline command

---
+++ RamokSelfbot-Rewritten/Commands/Text/Underline.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace RamokSelfbot.Commands.Text
+{
+    [Command("underline", "Underline the text - TEXT")]
+    class underline : CommandBase
+    {
+        [Parameter("text", false)]
+        public string text { get; set; }
+        public override async void Execute()
+        {
+            if (RamokSelfbot.Utils.IsClient(Message))
+            {
+                await Message.EditAsync(new MessageEditProperties()
+                {
+                    Content = $"__{text}__"
+                });
+            }
+        }
+    }
+}
