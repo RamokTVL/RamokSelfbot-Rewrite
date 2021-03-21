d4aae790ea6bb9f84d44f50c798ba2dc094a354d
RamokTVL <steamxaiden@gmail.com>
2021-03-21 23:13:05 +0100

    Adding bold command

---
+++ RamokSelfbot-Rewritten/Commands/Text/Bold.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace RamokSelfbot.Commands.Text
+{
+    [Command("bold", "Set the text to bold! - TEXT")]
+    class Bold : CommandBase
+    {
+        [Parameter("text", false)]
+        public string text { get; set; }
+        public override async void Execute()
+        {
+            if (RamokSelfbot.Utils.IsClient(Message))
+            {
+                await Message.EditAsync(new MessageEditProperties()
+                {
+                    Content = $"**{text}**"
+                });
+            }
+        }
+    }
+}
