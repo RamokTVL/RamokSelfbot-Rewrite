f7dd863043277501586e81b7d35f8bc9270dfce5
RamokTVL <steamxaiden@gmail.com>
2021-03-21 23:13:30 +0100

    Adding Italic command

---
+++ RamokSelfbot-Rewritten/Commands/Text/Italic.cs
@@ -0,0 +1,23 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace RamokSelfbot.Commands.Text
+{
+    [Command("italic", "Set your text to italic - TEXT")]
+    class Italic : CommandBase
+    {
+        [Parameter("text", false)]
+        public string text { get; set; }
+        public override async void Execute()
+        {
+            if (RamokSelfbot.Utils.IsClient(Message))
+            {
+                await Message.EditAsync(new MessageEditProperties()
+                {
+                    Content = $"*{text}*"
+                });
+            }
+        }
+    }
+}
+
