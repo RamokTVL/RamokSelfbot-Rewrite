110c8c217bbf08b91a40783f41f1656b369c1776
RamokTVL <steamxaiden@gmail.com>
2021-03-21 23:13:13 +0100

    Adding bold crossed command

---
+++ RamokSelfbot-Rewritten/Commands/Text/BoldCrossed.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace RamokSelfbot.Commands.Text
+{
+    [Command("boldunderlined", "Set your text to Bold Underlined - TEXT")]
+    class boldcrossed : CommandBase
+    {
+        [Parameter("text")]
+        public string text { get; set; }
+        public override async void Execute()
+        {
+            if (RamokSelfbot.Utils.IsClient(Message))
+            {
+                await Message.EditAsync(new MessageEditProperties()
+                {
+                    Content = $"__**{text}**__"
+                });
+            }
+        }
+    }
+}
